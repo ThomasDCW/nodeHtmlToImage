a1b07e3e7b8ed396f3a91c6806be13b60e1dd55c
Thomas Di Cola <thomas.dicola@user-agency.com>
2024-06-04 12:09:56 +0200

    generate image with a selector

--- index.js
+++ index.js
@@ -1,35 +1,45 @@
 const nodeHtmlToImage = require("node-html-to-image");
-const fs = require("fs");
+const puppeteer = require("puppeteer");
+
+async function getImageFromSelector(url, selector) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+  const image = await page.$eval(selector, (img) => img.src);
+  await browser.close();
+  return image;
+}
 
 const title = "Titre";
 const description = "Description de la vignette.";
-const image = fs.readFileSync("./images/image.png");
-const base64Image = Buffer.from(image).toString("base64");
-const dataURI = `data:image/jpeg;base64,${base64Image}`;
+const url = "https://github.com/ThomasDCW";
+const selector = ".avatar";
 
-nodeHtmlToImage({
-  output: `./generated/thumbnail_${Date.now()}.png`,
-  html: `
-    <html>
-      <style>
-        body {
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          justify-content: center;
-        }
-      </style>
-      <body>
-        <h1>${title}</h1>
-        <p>${description}</p>
-        <img src="{{imageSource}}" />
-      </body>
-    </html>`,
-  content: { imageSource: dataURI },
-})
-  .then(() => {
-    console.log("La vignette a été créée avec succès!");
+getImageFromSelector(url, selector).then((dataURI) => {
+  nodeHtmlToImage({
+    output: `./generated/thumbnail_${Date.now()}.png`,
+    html: `
+      <html>
+        <style>
+          body {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+          }
+        </style>
+        <body>
+          <h1>${title}</h1>
+          <p>${description}</p>
+          <img src="{{imageSource}}" />
+        </body>
+      </html>`,
+    content: { imageSource: dataURI },
   })
-  .catch((error) => {
-    console.error("Erreur lors de la création de la vignette:", error);
-  });
+    .then(() => {
+      console.log("La vignette a été créée avec succès!");
+    })
+    .catch((error) => {
+      console.error("Erreur lors de la création de la vignette:", error);
+    });
+});
